4d1ba8d2ff89c3f78fb06fdaef96e0c56deaa00a
BrendaStefany <brenda.stefany.cavalcanti@gmail.com>
2023-11-09 14:26:23 -0300

    Projeto API Via Cep

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    cep: '',
+    rua: '',
+    bairro: '',
+    cidade: '',
+    uf: '',
+  });
+
+  const checkCEP = (e) => {
+    const cep = e.target.value.replace(/\D/g, '');
+    if (!e.target.value) return;
+    console.log(cep);
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setFormData({
+          ...formData,
+          rua: data.logradouro,
+          bairro: data.bairro,
+          cidade: data.localidade,
+          uf: data.uf,
+        });
+      }).catch((err) => console.log(err));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form >
+      <label>
+        CEP:
+        <input
+          type="text"
+          value={formData.cep}
+          onChange={e => setFormData({ ...formData, cep: e.target.value })}
+          onBlur={checkCEP}
+        />
+      </label>
+      <label>
+        Rua:
+        <input type="text" value={formData.rua} />
+      </label>
+      <label>
+        Bairro:
+        <input type="text" value={formData.bairro} />
+      </label>
+      <label>
+        Cidade:
+        <input type="text" value={formData.cidade} />
+      </label>
+      <label>
+        Estado:
+        <input type="text" value={formData.uf} />
+      </label>
+    </form>
   );
 }
 
